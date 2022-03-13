f68df2abaaea9bf9b9b303dbdff50dec5c62e66a
oguzn.sari <oguzn.sari@gmail.com>
2022-03-13 19:25:47 +0300

    logo ÄuÄpdate correct commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,12 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <div>freeCodeCamp</div>
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/svg/freeCodeCampLogo.svg"
+                              style="height: 20px;
+                              border-right: 1px solid #333;
+                              padding-right: 5px !important;"></div>
+                    <div style="padding-left: 5px !important;" >freeCodeGram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
